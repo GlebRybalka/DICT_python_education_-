22b3424c7b4764351c3d66215e31fee14c316210
Gleb <h.s.rybalka@student.khai.edu>
2025-03-10 22:50:29 +0200

    fix and add functions

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -5,17 +5,33 @@ import sys
 def random_arithmetic():
     operator = random.choice(["+", "-", "*"])
     task = f"{random.randint(2, 9)} {operator} {random.randint(2, 9)}"
-    user_answer = input(f"{task}\n>")
-    correct_answer = eval(task)
-    return user_answer, correct_answer
+    u_a = input(f"{task}\n>")
+    c_a = eval(task)
+    return u_a, c_a
+
 
 def random_squaring():
     x = random.randint(11, 29)
     task = f"{x} * {x}"
-    user_answer = input(f"{task}\n>")
-    correct_answer = eval(task)
-    return user_answer, correct_answer
+    u_a = input(f"{task}\n>")
+    c_a = eval(task)
+    return u_a, c_a
 
+
+def make_result_message(name, num1, num2, text):
+    message = f"{name}: {num1}/5 in level {num2} {text}.\n"
+    return message
+
+
+def save_result_to_file(message):
+    result_file = open("results.txt", "a")
+    result_file.write(message)
+
+
+user_answer = None
+correct_answer = None
+level_description = None
+print(user_answer, type(user_answer), user_answer == False)
 level = input("Which level do you want? Enter a number:\n"
               "1 - simple operations with numbers 2-9\n"
               "2 - integral squares of 11-29\n>")
@@ -34,8 +50,8 @@ while task_counter > 0:
     while re.match('[a-zA-Z`~!@#$%^&*()+=,./;<>?:]', user_answer) or user_answer == '':
         print("Incorrect format.")
         user_answer = input(">")
-    else: user_answer = int(user_answer)
-    if user_answer == correct_answer:
+    else: user_answer_int = int(user_answer)
+    if user_answer_int == correct_answer:
         right_answer_counter += 1
         print("Right!")
     else:
@@ -45,8 +61,7 @@ save_result = input(f"Your mark is {right_answer_counter}/5."
                     f"Would you like to save your result to the file? Enter yes or no.\n>")
 if save_result == 'yes' or save_result == 'Yes' or save_result == 'YES' or save_result == 'y' or save_result == 'Y':
     user_name = input("What is your name?\n>")
-    print(f"{user_name}: {right_answer_counter}/5 in level {level} ({level_description})")
-    print('The results are saved in "results.txt".')
-    result_file = open("results.txt", "a")
-    result_file.write(f"{user_name}: {right_answer_counter}/5 in level {level} ({level_description})\n")
+    result_message = make_result_message(user_name, right_answer_counter, level, level_description)
+    print("The results are saved in 'results.txt'.")
+    save_result_to_file(result_message)
 else: sys.exit()
